532b39689c07281403eda7eeadbf34a8c98c9cd6
Abhishek Sharma <108112397+abhishek020621@users.noreply.github.com>
2023-08-09 00:23:49 -0700

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"64d33d1c477c3a0019b6608a",
+				"64d33d1c477c3a0019b6608d"
+			]
+		},
+		"64d33d1c477c3a0019b6608a": {
+			"id": "64d33d1c477c3a0019b6608a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"64d33d1c477c3a0019b6608d": {
+			"id": "64d33d1c477c3a0019b6608d",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
